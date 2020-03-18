9fbb6410b887ee19efcbe23128068d5c830bb5da
Helga Nike <d4rkhelga@gmail.com>
2020-03-18 13:01:47 +0200

    Bug fixing - Responsive

--- resources/scripts/partials/card-actions.js
+++ resources/scripts/partials/card-actions.js
@@ -6,7 +6,13 @@
 
     $('.card-actions__action-link', cardActionBlock).on('click', function() {
       let parent = $(this).parents('.card-actions');
+      $('.card-actions').not(parent).find('.card-actions__items').hide();
       $('.card-actions__items', parent).toggle();
+    });
+
+    $('.card-actions__action').on('click', function() {
+      console.log('333');
+      $(this).parents('.card-actions').find('.card-actions__items').hide();
     });
 
     /*Клик вне элемента*/
